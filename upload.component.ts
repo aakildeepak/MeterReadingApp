de513dc8f20f72302d4e4145738d2b22ab7b9ccb
aakildeepak <60352423+aakildeepak@users.noreply.github.com>
2022-02-27 14:27:46 +0000

    Add files via upload

---
+++ upload.component.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+import { UploadService } from 'src/services/upload.service';
+import { UploadResult } from '../models/upload-result';
+
+@Component({
+    selector: 'app-upload',
+    templateUrl: './upload.component.html',
+    styleUrls: ['./upload.component.css']
+})
+export class UploadComponent {
+    validationErrors: any;
+    error = false;
+    success = false;
+    fileToUpload: any;
+    uploadResult: UploadResult;
+
+    constructor(private uploadService: UploadService) {
+
+    }
+    
+    @ViewChild('inputFileTarget') inputFileTarget: any;
+
+    updated($event) {
+        this.error = false;
+        this.success = false;
+        const files = $event.target.files || $event.srcElement.files;
+        const fileName = files[0];
+        if (this.isCsvFile(fileName.name)) {
+            this.fileToUpload = fileName;
+        } else {
+            this.resetUpload();
+        }
+    }
+
+    getExtension(filename) {
+        const parts = filename.split('.');
+        return parts[parts.length - 1];
+    }
+
+    isCsvFile(filename) {
+        const ext = this.getExtension(filename);
+        return ext.toLowerCase() === 'csv';
+    }
+
+    resetUpload() {
+        this.inputFileTarget.nativeElement.value = '';
+    }
+
+    fileSelected(): boolean {
+        return Boolean(this.fileToUpload);
+    }
+    
+    upload(): void {
+        if (!this.fileSelected()) {
+            return;
+        }
+        const formData = new FormData();
+        formData.append('file', this.fileToUpload);
+
+        this.uploadService.upload(formData)
+            .subscribe((result) => {
+                this.success = true;
+                this.uploadResult = result;
+            });
+    }    
+}
